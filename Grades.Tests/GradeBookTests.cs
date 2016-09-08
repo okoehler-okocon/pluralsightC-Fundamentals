b4ddcb00ded2e823d70bf7751670b6b04aa62996
nm_te <nm_te@DESKTOP-O8DU591>
2016-09-08 18:24:10 +0200

    End Module 3

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grades;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(90, result.HighestGrade);
+        }
+
+        [TestMethod]
+        public void ComputesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(10, result.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComputesAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            book.AddGrade(29.6f);
+            book.AddGrade(57);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(46.65, result.AverageGrade,0.01);
+        }
+    }
+}
